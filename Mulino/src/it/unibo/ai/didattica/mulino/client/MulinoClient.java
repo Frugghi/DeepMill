3148ee945417b2c2c6d41bc012297e4ce4988c61
Tommaso Madonia <tommaso@madonia.me>
2015-04-30 20:00:56 +0200

    Use spaces instead of tab for indentation

--- Mulino/src/it/unibo/ai/didattica/mulino/client/MulinoClient.java
+++ Mulino/src/it/unibo/ai/didattica/mulino/client/MulinoClient.java
@@ -21,90 +21,90 @@ import it.unibo.ai.didattica.mulino.engine.TCPMulino;
 
 public class MulinoClient {
 
-	public enum Player {
-		HUMAN ("Human"),
-		IA ("DeepMill");
-		private final String name;
-		private Player(String s) { name = s; }
-		public boolean equalsName(String otherName){ return (otherName == null)? false:name.equals(otherName); }
-		public String toString(){ return name; }
-	}
+    public enum Player {
+        HUMAN ("Human"),
+        IA ("DeepMill");
+        private final String name;
+        private Player(String s) { name = s; }
+        public boolean equalsName(String otherName){ return (otherName == null)? false:name.equals(otherName); }
+        public String toString(){ return name; }
+    }
 
-	private State.Checker player;
-	private Socket playerSocket;
-	private ObjectInputStream in;
-	private ObjectOutputStream out;
-	
-	public MulinoClient(State.Checker player) throws UnknownHostException, IOException {
-		this.player = player;
-		int port = 0;
-		switch (player) {
-			case WHITE:
-				port = TCPMulino.whiteSocket;
-				break;
-			case BLACK:
-				port = TCPMulino.blackSocket;
-				break;
-			default:
-				System.exit(5);
-		}
-		playerSocket = new Socket("localhost", port);
-		out = new ObjectOutputStream(playerSocket.getOutputStream());
-		in = new ObjectInputStream(new BufferedInputStream(playerSocket.getInputStream()));
-	}
+    private State.Checker player;
+    private Socket playerSocket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 
-	public void write(Action action) throws IOException, ClassNotFoundException {
-		out.writeObject(action);
-	}
-	
-	public State read() throws ClassNotFoundException, IOException {
-		return (State) in.readObject();
-	}
-	
-	public State.Checker getPlayer() { return player; }
-	public void setPlayer(State.Checker player) { this.player = player; }
+    public MulinoClient(State.Checker player) throws UnknownHostException, IOException {
+        this.player = player;
+        int port = 0;
+        switch (player) {
+            case WHITE:
+                port = TCPMulino.whiteSocket;
+                break;
+            case BLACK:
+                port = TCPMulino.blackSocket;
+                break;
+            default:
+                System.exit(5);
+        }
+        playerSocket = new Socket("localhost", port);
+        out = new ObjectOutputStream(playerSocket.getOutputStream());
+        in = new ObjectInputStream(new BufferedInputStream(playerSocket.getInputStream()));
+    }
 
-	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
+    public void write(Action action) throws IOException, ClassNotFoundException {
+        out.writeObject(action);
+    }
 
-		if (args.length == 0) {
-			System.out.println("You must specify which player you are (Wthie or Black)!");
-			System.exit(-1);
-		}
-		System.out.print("Selected client:");
-		for (String arg : args) {
-			System.out.print(" " + arg);
-		}
-		System.out.println();
+    public State read() throws ClassNotFoundException, IOException {
+        return (State) in.readObject();
+    }
 
-		State.Checker playerColor = "White".equalsIgnoreCase(args[0]) ? State.Checker.WHITE : State.Checker.BLACK;
-		Player player = Player.IA;
-		if (args.length > 1) {
-			player = "Human".equalsIgnoreCase(args[1]) ? Player.HUMAN : Player.IA;
-		}
+    public State.Checker getPlayer() { return player; }
+    public void setPlayer(State.Checker player) { this.player = player; }
 
-		boolean myTurn = playerColor == State.Checker.WHITE;
-		String actionString = "";
-		Action action = null;
-		State currentState = null;
-		BufferedReader in = new BufferedReader( new InputStreamReader(System.in));
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
 
-		Minimax<MillMove> ia = new MillMinimax(Minimax.Algorithm.MINIMAX);
-		MillMove move;
-		int depth = 8;
+        if (args.length == 0) {
+            System.out.println("You must specify which player you are (Wthie or Black)!");
+            System.exit(-1);
+        }
+        System.out.print("Selected client:");
+        for (String arg : args) {
+            System.out.print(" " + arg);
+        }
+        System.out.println();
 
-		MulinoClient client = new MulinoClient(playerColor);
-		System.out.println("Hello " + player.toString() + "!");
-		System.out.println("You are player " + client.getPlayer().toString() + "!");
-		System.out.println("Current state:");
-		currentState = client.read();
-		System.out.println(currentState.toString());
+        State.Checker playerColor = "White".equalsIgnoreCase(args[0]) ? State.Checker.WHITE : State.Checker.BLACK;
+        Player player = Player.IA;
+        if (args.length > 1) {
+            player = "Human".equalsIgnoreCase(args[1]) ? Player.HUMAN : Player.IA;
+        }
 
-		while (true) {
-			if (!myTurn) {
-				System.out.println("Waiting for your opponent move...");
-				currentState = client.read();
-				System.out.println("Your Opponent did his move, and the result is: ");
-				System.out.println(currentState.toString());
+        boolean myTurn = playerColor == State.Checker.WHITE;
+        String actionString = "";
+        Action action = null;
+        State currentState = null;
+        BufferedReader in = new BufferedReader( new InputStreamReader(System.in));
+
+        Minimax<MillMove> ia = new MillMinimax(Minimax.Algorithm.MINIMAX);
+        MillMove move;
+        int depth = 8;
+
+        MulinoClient client = new MulinoClient(playerColor);
+        System.out.println("Hello " + player.toString() + "!");
+        System.out.println("You are player " + client.getPlayer().toString() + "!");
+        System.out.println("Current state:");
+        currentState = client.read();
+        System.out.println(currentState.toString());
+
+        while (true) {
+            if (!myTurn) {
+                System.out.println("Waiting for your opponent move...");
+                currentState = client.read();
+                System.out.println("Your Opponent did his move, and the result is: ");
+                System.out.println(currentState.toString());
 
                 if (player == Player.IA) {
                     for (String position : currentState.getPositions()) {
@@ -128,69 +128,69 @@ public class MulinoClient {
 
                     System.out.println("DEEPMILL DEBUG: \n" + ia.toString());
                 }
-			}
-			System.out.println("Player " + client.getPlayer().toString() + ", do your move: ");
-			switch (player) {
-				case IA:
-					move = ia.getBestMove(depth);
+            }
+            System.out.println("Player " + client.getPlayer().toString() + ", do your move: ");
+            switch (player) {
+                case IA:
+                    move = ia.getBestMove(depth);
                     System.out.println("DEEPMILL DEBUG: " + move.toString());
-					if (move == null) {
-						actionString = "GGWP";
-					} else {
-						ia.makeMove(move);
-						actionString = move.toStandardMove();
-					}
+                    if (move == null) {
+                        actionString = "GGWP";
+                    } else {
+                        ia.makeMove(move);
+                        actionString = move.toStandardMove();
+                    }
 
                     System.out.println(actionString);
                     System.out.println("DEEPMILL DEBUG: \n" + ia.toString());
                     break;
-				case HUMAN:
-					actionString = in.readLine();
-					break;
-			}
+                case HUMAN:
+                    actionString = in.readLine();
+                    break;
+            }
 
-			switch (currentState.getCurrentPhase()) {
-				case FIRST:
-					Phase1Action phase1Action = new Phase1Action();
-					phase1Action.setPutPosition(actionString.substring(0, 2));
-					if (actionString.length() == 4) {
-						phase1Action.setRemoveOpponentChecker(actionString.substring(2, 4));
-					} else {
-						phase1Action.setRemoveOpponentChecker(null);
-					}
-					action = phase1Action;
-					break;
-				case SECOND:
-					Phase2Action phase2Action = new Phase2Action();
-					phase2Action.setFrom(actionString.substring(0, 2));
-					phase2Action.setTo(actionString.substring(2, 4));
-					if (actionString.length() == 6) {
-						phase2Action.setRemoveOpponentChecker(actionString.substring(4, 6));
-					} else {
-						phase2Action.setRemoveOpponentChecker(null);
-					}
-					action = phase2Action;
-					break;
-				case FINAL:
-					PhaseFinalAction phaseFinalAction = new PhaseFinalAction();
-					phaseFinalAction.setFrom(actionString.substring(0, 2));
-					phaseFinalAction.setTo(actionString.substring(2, 4));
-					if (actionString.length() == 6) {
-						phaseFinalAction.setRemoveOpponentChecker(actionString.substring(4, 6));
-					} else {
-						phaseFinalAction.setRemoveOpponentChecker(null);
-					}
-					action = phaseFinalAction;
-					break;
-			}
-			client.write(action);
-			currentState = client.read();
-			System.out.println("Effect of your move: ");
-			System.out.println(currentState.toString());
-			myTurn = false;
-		}
-	}
+            switch (currentState.getCurrentPhase()) {
+                case FIRST:
+                    Phase1Action phase1Action = new Phase1Action();
+                    phase1Action.setPutPosition(actionString.substring(0, 2));
+                    if (actionString.length() == 4) {
+                        phase1Action.setRemoveOpponentChecker(actionString.substring(2, 4));
+                    } else {
+                        phase1Action.setRemoveOpponentChecker(null);
+                    }
+                    action = phase1Action;
+                    break;
+                case SECOND:
+                    Phase2Action phase2Action = new Phase2Action();
+                    phase2Action.setFrom(actionString.substring(0, 2));
+                    phase2Action.setTo(actionString.substring(2, 4));
+                    if (actionString.length() == 6) {
+                        phase2Action.setRemoveOpponentChecker(actionString.substring(4, 6));
+                    } else {
+                        phase2Action.setRemoveOpponentChecker(null);
+                    }
+                    action = phase2Action;
+                    break;
+                case FINAL:
+                    PhaseFinalAction phaseFinalAction = new PhaseFinalAction();
+                    phaseFinalAction.setFrom(actionString.substring(0, 2));
+                    phaseFinalAction.setTo(actionString.substring(2, 4));
+                    if (actionString.length() == 6) {
+                        phaseFinalAction.setRemoveOpponentChecker(actionString.substring(4, 6));
+                    } else {
+                        phaseFinalAction.setRemoveOpponentChecker(null);
+                    }
+                    action = phaseFinalAction;
+                    break;
+            }
+            client.write(action);
+            currentState = client.read();
+            System.out.println("Effect of your move: ");
+            System.out.println(currentState.toString());
+            myTurn = false;
+        }
+    }
 
 
-	
+
 }
