32e345564492377aa34a4baa86483f2d0d3f331d
Tommaso Madonia <tommaso@madonia.me>
2015-05-11 02:31:16 +0200

    Use byte instead of int

--- Mulino/src/it/unibo/ai/didattica/mulino/implementation/BitBoardMove.java
+++ Mulino/src/it/unibo/ai/didattica/mulino/implementation/BitBoardMove.java
@@ -4,32 +4,32 @@ import it.unibo.ai.didattica.mulino.domain.MillMove;
 
 public class BitBoardMove implements MillMove {
 
-    private int player;
+    private byte player;
 
-    private int to;
-    private int from;
-    private int remove;
+    private byte to;
+    private byte from;
+    private byte remove;
 
-    public BitBoardMove(int player, int from, int to, int remove) {
+    public BitBoardMove(byte player, byte from, byte to, byte remove) {
         this.player = player;
         this.from = from;
         this.to = to;
         this.remove = remove;
     }
 
-    public BitBoardMove(int player, int from, int to) {
-        this(player, from, to, Integer.MAX_VALUE);
+    public BitBoardMove(byte player, byte from, byte to) {
+        this(player, from, to, Byte.MAX_VALUE);
     }
 
-    public BitBoardMove(int player, int to) {
-        this(player, Integer.MAX_VALUE, to, Integer.MAX_VALUE);
+    public BitBoardMove(byte player, byte to) {
+        this(player, Byte.MAX_VALUE, to, Byte.MAX_VALUE);
     }
 
     public int getTo() {
         return to;
     }
 
-    public void setTo(int to) {
+    public void setTo(byte to) {
         this.to = to;
     }
 
@@ -37,7 +37,7 @@ public class BitBoardMove implements MillMove {
         return remove;
     }
 
-    public void setRemove(int remove) {
+    public void setRemove(byte remove) {
         this.remove = remove;
     }
 
@@ -45,16 +45,16 @@ public class BitBoardMove implements MillMove {
         return from;
     }
 
-    public void setFrom(int from) {
+    public void setFrom(byte from) {
         this.from = from;
     }
 
     public boolean isPutMove() {
-        return this.from == Integer.MAX_VALUE;
+        return this.from == Byte.MAX_VALUE;
     }
 
     public boolean isRemoveMove() {
-        return this.remove != Integer.MAX_VALUE;
+        return this.remove != Byte.MAX_VALUE;
     }
 
     @Override
@@ -90,64 +90,64 @@ public class BitBoardMove implements MillMove {
 
     private String bitPattern2string(int bitPattern) {
         switch (bitPattern) {
-            case BitBoardMinimax.A1: return "a1";
-            case BitBoardMinimax.A4: return "a4";
-            case BitBoardMinimax.A7: return "a7";
-            case BitBoardMinimax.B2: return "b2";
-            case BitBoardMinimax.B4: return "b4";
-            case BitBoardMinimax.B6: return "b6";
-            case BitBoardMinimax.C3: return "c3";
-            case BitBoardMinimax.C4: return "c4";
-            case BitBoardMinimax.C5: return "c5";
-            case BitBoardMinimax.D1: return "d1";
-            case BitBoardMinimax.D2: return "d2";
-            case BitBoardMinimax.D3: return "d3";
-            case BitBoardMinimax.D5: return "d5";
-            case BitBoardMinimax.D6: return "d6";
-            case BitBoardMinimax.D7: return "d7";
-            case BitBoardMinimax.E3: return "e3";
-            case BitBoardMinimax.E4: return "e4";
-            case BitBoardMinimax.E5: return "e5";
-            case BitBoardMinimax.F2: return "f2";
-            case BitBoardMinimax.F4: return "f4";
-            case BitBoardMinimax.F6: return "f6";
-            case BitBoardMinimax.G1: return "g1";
-            case BitBoardMinimax.G4: return "g4";
-            case BitBoardMinimax.G7: return "g7";
+            case BitBoardMinimax._A1: return "a1";
+            case BitBoardMinimax._A4: return "a4";
+            case BitBoardMinimax._A7: return "a7";
+            case BitBoardMinimax._B2: return "b2";
+            case BitBoardMinimax._B4: return "b4";
+            case BitBoardMinimax._B6: return "b6";
+            case BitBoardMinimax._C3: return "c3";
+            case BitBoardMinimax._C4: return "c4";
+            case BitBoardMinimax._C5: return "c5";
+            case BitBoardMinimax._D1: return "d1";
+            case BitBoardMinimax._D2: return "d2";
+            case BitBoardMinimax._D3: return "d3";
+            case BitBoardMinimax._D5: return "d5";
+            case BitBoardMinimax._D6: return "d6";
+            case BitBoardMinimax._D7: return "d7";
+            case BitBoardMinimax._E3: return "e3";
+            case BitBoardMinimax._E4: return "e4";
+            case BitBoardMinimax._E5: return "e5";
+            case BitBoardMinimax._F2: return "f2";
+            case BitBoardMinimax._F4: return "f4";
+            case BitBoardMinimax._F6: return "f6";
+            case BitBoardMinimax._G1: return "g1";
+            case BitBoardMinimax._G4: return "g4";
+            case BitBoardMinimax._G7: return "g7";
             default:
-                if (bitPattern != Integer.MAX_VALUE) {
+                if (bitPattern != Byte.MAX_VALUE) {
                     System.err.println("Unknown move: " + bitPattern);
                 }
                 return "";
         }
     }
 
-    protected static int string2bitPattern(String position) {
+    public static byte string2byte(String position) {
         switch (position.toLowerCase()) {
-            case "a1": return BitBoardMinimax.A1;
-            case "a4": return BitBoardMinimax.A4;
-            case "a7": return BitBoardMinimax.A7;
-            case "b2": return BitBoardMinimax.B2;
-            case "b4": return BitBoardMinimax.B4;
-            case "b6": return BitBoardMinimax.B6;
-            case "c3": return BitBoardMinimax.C3;
-            case "c4": return BitBoardMinimax.C4;
-            case "c5": return BitBoardMinimax.C5;
-            case "d1": return BitBoardMinimax.D1;
-            case "d2": return BitBoardMinimax.D2;
-            case "d3": return BitBoardMinimax.D3;
-            case "d5": return BitBoardMinimax.D5;
-            case "d6": return BitBoardMinimax.D6;
-            case "d7": return BitBoardMinimax.D7;
-            case "e3": return BitBoardMinimax.E3;
-            case "e4": return BitBoardMinimax.E4;
-            case "e5": return BitBoardMinimax.E5;
-            case "f2": return BitBoardMinimax.F2;
-            case "f4": return BitBoardMinimax.F4;
-            case "f6": return BitBoardMinimax.F6;
-            case "g1": return BitBoardMinimax.G1;
-            case "g4": return BitBoardMinimax.G4;
-            case "g7": return BitBoardMinimax.G7;
+            case "a1": return BitBoardMinimax._A1;
+            case "a4": return BitBoardMinimax._A4;
+            case "a7": return BitBoardMinimax._A7;
+            case "b2": return BitBoardMinimax._B2;
+            case "b4": return BitBoardMinimax._B4;
+            case "b6": return BitBoardMinimax._B6;
+            case "c3": return BitBoardMinimax._C3;
+            case "c4": return BitBoardMinimax._C4;
+            case "c5": return BitBoardMinimax._C5;
+            case "d1": return BitBoardMinimax._D1;
+            case "d2": return BitBoardMinimax._D2;
+            case "d3": return BitBoardMinimax._D3;
+            case "d5": return BitBoardMinimax._D5;
+            case "d6": return BitBoardMinimax._D6;
+            case "d7": return BitBoardMinimax._D7;
+            case "e3": return BitBoardMinimax._E3;
+            case "e4": return BitBoardMinimax._E4;
+            case "e5": return BitBoardMinimax._E5;
+            case "f2": return BitBoardMinimax._F2;
+            case "f4": return BitBoardMinimax._F4;
+            case "f6": return BitBoardMinimax._F6;
+            case "g1": return BitBoardMinimax._G1;
+            case "g4": return BitBoardMinimax._G4;
+            case "g7": return BitBoardMinimax._G7;
             default:
                 System.err.println("Unknown move: " + position);
                 return 0;
