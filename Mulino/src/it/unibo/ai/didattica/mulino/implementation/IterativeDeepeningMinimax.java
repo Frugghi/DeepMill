84aef1c405385fd1b012da92929f9223ac86f4cb
Tommaso Madonia <tommaso@madonia.me>
2015-05-11 02:28:30 +0200

    Why we get null moves?

--- Mulino/src/it/unibo/ai/didattica/mulino/implementation/IterativeDeepeningMinimax.java
+++ Mulino/src/it/unibo/ai/didattica/mulino/implementation/IterativeDeepeningMinimax.java
@@ -41,20 +41,29 @@ public abstract class IterativeDeepeningMinimax<M extends Move, T extends Compar
     public M getBestMove(final int maxDepth) {
         this.purgeHeuristic(2);
 
+        long time = System.currentTimeMillis();
+
         M bestMove = null;
         for (this.depth = 1; this.depth != maxDepth && !this.shouldAbort(); this.depth++) {
             System.out.print("Depth " + this.depth);
 
             M move = super.getBestMove(this.depth);
 
-            if (!this.shouldAbort()) {
+            if (move == null) {
+                System.err.println("Best move is null at depth " + this.depth);
+                this.purgeHeuristic(0);
+                move = super.getBestMove(this.depth);
+            }
+
+            if (!this.shouldAbort() && move != null) {
                 bestMove = move;
 
                 this.makeMove(bestMove);
                 this.setAbort(this.isOver());
                 this.unmakeMove(bestMove);
 
-                System.out.println("... done! " + this.getNodesCount() + " nodes evaluated.");
+                System.out.print("... done! " + (System.currentTimeMillis() - time) + "ms ");
+                this.printStatistics();
             } else {
                 System.out.println("... aborted!");
             }
